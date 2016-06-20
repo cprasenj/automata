383e4e713a5be2dfd12239f5d3fe1810ba8067fd
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2016-06-20 11:11:25 +0530

    refactor<Prasenjit>

--- util.js
+++ util.js
@@ -1,6 +1,10 @@
 var util = {};
 var _ = require("lodash");
 
+util.contains = function(array, element) {
+  return array.indexOf(element) >= 0;
+}
+
 util.allCombinations = function combinations(list) {
     var combinator = function(active, rest, bucket) {
         if (!active.length && !rest.length)
@@ -18,7 +22,7 @@ util.allCombinations = function combinations(list) {
 
 util.subSet = function(oneStateSet, aNotherStateSet) {
   return (aNotherStateSet.length > 0) && aNotherStateSet.every(function(state) {
-    return oneStateSet.indexOf(state) >= 0;
+    return util.contains(oneStateSet, state);
   });
 }
 
@@ -30,10 +34,6 @@ util.evalNestedValue = function(object, keys) {
 
 util.sortedJoin = function(list) {
   return list.sort().join('');
-}
-
-util.contains = function(array, element) {
-  return array.indexOf(element) >= 0;
 }
 
 exports.util = util;
